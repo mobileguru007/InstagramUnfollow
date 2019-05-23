3bf709de6d8828c0ab8eb626ea627d7642ce2c49
zhezui928 <40060741+zhezui928@users.noreply.github.com>
2019-05-24 02:40:12 +0800

    update

--- app/src/main/java/com/it_tech613/zhe/instagramunfollow/fragment/FaqFragment.java
+++ app/src/main/java/com/it_tech613/zhe/instagramunfollow/fragment/FaqFragment.java
@@ -17,33 +17,33 @@ import com.it_tech613.zhe.instagramunfollow.utils.PreferenceManager;
 
 
 public class FaqFragment extends Fragment {
-    public static FaqFragment newInstance() {
-        FaqFragment fragment = new FaqFragment();
-        return fragment;
+  public static FaqFragment newInstance() {
+    FaqFragment fragment = new FaqFragment();
+    return fragment;
+  }
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    View view= inflater.inflate(R.layout.fragment_privacy_policy, container, false);
+    initWidget(view);
+    return view;
+  }
+  @SuppressLint("SetJavaScriptEnabled")
+  private void initWidget(View view){
+    WebView webView = (WebView)view.findViewById(R.id.webview);
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.loadUrl(PreferenceManager.webviewUri);
+    webView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      // chromium, enable hardware acceleration
+      webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+    } else {
+      // older android version, disable hardware acceleration
+      webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
     }
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_privacy_policy, container, false);
-        initWidget(view);
-        return view;
-    }
-    @SuppressLint("SetJavaScriptEnabled")
-    private void initWidget(View view){
-        WebView webView = (WebView)view.findViewById(R.id.webview);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl(PreferenceManager.webviewUri);
-        webView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            // chromium, enable hardware acceleration
-            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
-        } else {
-            // older android version, disable hardware acceleration
-            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        }
-        webView.setWebViewClient(new WebViewClient());
-        webView.setBackgroundColor(Color.TRANSPARENT);
-        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-    }
+    webView.setWebViewClient(new WebViewClient());
+    webView.setBackgroundColor(Color.TRANSPARENT);
+    webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+  }
 }
